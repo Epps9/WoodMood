736f748c8f17b11d90377302af2d21f3be8d7936
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-02-04 22:20:32 +0100

    Improve Layout component

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {GiLindenLeaf} from "react-icons/gi";
+import {FaFacebookSquare, FaInstagram, FaLinkedin} from "react-icons/fa"
 
 import clsx from 'clsx';
 
@@ -9,30 +10,67 @@ import clsx from 'clsx';
 
 import styles from './MainLayout.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <div className={styles.topBar}>
-      <button >MENU</button>
-      <div className={styles.logo}>
-        <GiLindenLeaf className={styles.leaf} />      
-        <h1>Wood Mood</h1>
+class Component extends React.Component {
+
+  constructor() {
+    super ()
+    this.state = {
+      showMe: true,
+    }
+  }
+
+  toggleMenu(){
+    this.setState({
+      showMe: !this.state.showMe
+    })
+  }
+
+
+  render() {
+
+    const {children, className} = this.props;
+
+    return (
+      <div className={clsx(className, styles.root)}>
+        <div className={styles.topBar}>
+          <button className={styles.menuButton} onClick={() => this.toggleMenu()}>MENU</button>
+          <div className={styles.logo}>
+            <GiLindenLeaf className={styles.leaf} />      
+            <h1>Wood Mood</h1>
+          </div>
+          <div className={styles.sideOptions}>
+            <a href="#">Register</a>
+            <a href="#">Log in</a>
+            <a href="#">Cart</a>
+          </div>
+        </div>
+        
+          {this.state.showMe?
+          <div className={styles.menu}>
+            <a href="#">PRODUCTS</a>
+            <a href="#">watches</a>
+            <a href="#">bracelets</a>
+            <a href="#">other</a>
+          </div>
+          :null
+          }
+        <div className={styles.footer}>
+          <div className={styles.info}>
+            <a>About us</a>
+            <a>Delivery</a>
+            <a>Contact</a>
+          </div>
+          <div className={styles.socialMedia}>
+            <a><FaFacebookSquare className={styles.socialMediaIcon}/></a>
+            <a><FaInstagram className={styles.socialMediaIcon}/></a>
+            <a><FaLinkedin className={styles.socialMediaIcon}/></a>
+
+          </div>
+        </div>
       </div>
-      <div className={styles.sideOptions}>
-        <a href="#">Register</a>
-        <a href="#">Log in</a>
-        <a href="#">Cart</a>
-      </div>
-    </div>
-    <div className={styles.menu}>
-      <a href="#">PRODUCTS</a>
-      <a href="#">watches</a>
-      <a href="#">bracelets</a>
-      <a href="#">other</a>
-    </div>
-    
-    {children}
-  </div>
-);
+    )
+  }
+};
 
 Component.propTypes = {
   children: PropTypes.node,
